7b16636170d6d3b1952d62311de97c04ca239a72
gokuhieu <84957461+gokuhieu@users.noreply.github.com>
2021-06-14 12:50:44 +0700

    Update webcaculatorserver.js

--- webcaculatorserver.js
+++ webcaculatorserver.js
@@ -19,14 +19,14 @@ app.get("/", (req, res) => {
     var result1=0;
     q = url.parse(req.url, true);
     var data=q.query;
-    const a =parseInt(data.a);
-    const b= parseInt(data.b);
-    const p =data.p;
-    result1 = dt.calc(a,b,p);
+    var a =parseInt(data.a);
+    var b= parseInt(data.b);
+    var p =data.p;
+   
     if(a==null&&b==null)
     {
-       result1= result1.toString();
     }else{
+        result1 = dt.calc(a,b,p);
     result1= a.toString()+" "+p+" "+b.toString()+" = " + result1.toString();
     }
     
