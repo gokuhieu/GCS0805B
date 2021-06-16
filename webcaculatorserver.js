9ea7d5646bc5a192d4884bcdd54eb131f9312e0c
gokuhieu <84957461+gokuhieu@users.noreply.github.com>
2021-06-16 11:20:10 +0700

    Update webcaculatorserver.js

--- webcaculatorserver.js
+++ webcaculatorserver.js
@@ -12,11 +12,7 @@ app.engine('html', require('ejs').renderFile);
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(express.static(path.join(__dirname, 'public')))
 
-router.get("/", (req,res) =>{
-    res.render("./home.html");
-    
-    });
-app.get('/home',(req,res)=>{
+app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./home.html'))
 
 })
