c4b604f48dc3fcfc4608fc3953001f82da72778a
gokuhieu <84957461+gokuhieu@users.noreply.github.com>
2021-06-27 21:19:47 +0700

    v1

--- product.js
+++ product.js
@@ -1,16 +1,8 @@
 
 
 var queryresult;
-const connect =(query) =>{
-    const connection = require('pg').Pool;
-    const myconect = new connection({
-        host : 'ec2-174-129-225-160.compute-1.amazonaws.com',
-        database : 'dfd8gcsog7njl7',
-        user : 'zzdduyaaxgfqab',
-        password : '0493727bcbcc2f72994bbedb01f5bfe1360109bc5c66505f7c18dc47e2a1f151',
-        port : 5432,
-        });
-    myconect.query(query,err,result =>{
+
+    myconect.query("SELECT * FROM public.product",err,result =>{
         if(err)
         {
         console.log(err);
@@ -19,6 +11,4 @@ const connect =(query) =>{
         queryresult=result;
         console.log(result)
         });
-        return result;
-    }
-exports.connect=connect;
+
--- webcaculatorserver.js
+++ webcaculatorserver.js
@@ -1,5 +1,4 @@
 
-var connection = require('./product.js');
 const express = require("express");
 const app = express();
 var url = require('url');
@@ -9,6 +8,14 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 var fs = require('fs');
 const { connect } = require('./product.js');
+const connection = require('pg').Pool;
+const myconect = new connection({
+    host : 'ec2-174-129-225-160.compute-1.amazonaws.com',
+    database : 'dfd8gcsog7njl7',
+    user : 'zzdduyaaxgfqab',
+    password : '0493727bcbcc2f72994bbedb01f5bfe1360109bc5c66505f7c18dc47e2a1f151',
+    port : 5432,
+    });
 app.engine('html', require('ejs').renderFile);
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -40,9 +47,15 @@ app.get('/addproduct/',(req,res)=>{
     const pprice =data.pprice;
     const cateid= data.cateid;
     query ="insert into public.product values('"+pid+"'"+",'"+pname+"'"+",'"+cateid+"'"+",'"+pprice+"')";
-    connection.connect(query);
+    myconect.query(query,err,result =>{
+        if(err)
+        {
+        console.log(err);
+        return;
+        }
+        console.log(result)
+    });
     res.sendFile(path.resolve(__dirname,'./home.html'))
-    
 })
 
 
