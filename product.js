a22c32b0210342e537289a2e49264586f5ff9c47
gokuhieu <84957461+gokuhieu@users.noreply.github.com>
2021-06-28 02:45:20 +0700

    Update product.js

--- product.js
+++ product.js
@@ -7,7 +7,6 @@ const myconect = new connection({
     database : 'dfd8gcsog7njl7',
     password : '0493727bcbcc2f72994bbedb01f5bfe1360109bc5c66505f7c18dc47e2a1f151',
     port : 5432,
-    ssl : on
     });
 const connect =(query1) =>{
     myconect.query(query1,(err,result) =>{
