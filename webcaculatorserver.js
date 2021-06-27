106566c7577410c87c57c822e4d64a5f60aaefae
gokuhieu <84957461+gokuhieu@users.noreply.github.com>
2021-06-28 02:07:32 +0700

    Update webcaculatorserver.js

--- webcaculatorserver.js
+++ webcaculatorserver.js
@@ -16,7 +16,7 @@ const connection = require('pg').Pool;
 const { on } = require("events");
 const myconect = new connection({
     host : 'ec2-174-129-225-160.compute-1.amazonaws.com',
-    database : 'dfd8gcsog7njl7',
+    database : 'public.dfd8gcsog7njl7',
     user : 'zzdduyaaxgfqab',
     password : '0493727bcbcc2f72994bbedb01f5bfe1360109bc5c66505f7c18dc47e2a1f151',
     port : 5432,
