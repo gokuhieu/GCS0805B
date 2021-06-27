d6d340a06a0638fc9fc5897375d11e374c3e4d46
gokuhieu <84957461+gokuhieu@users.noreply.github.com>
2021-06-28 02:44:23 +0700

    Update product.js

--- product.js
+++ product.js
@@ -1,15 +1,15 @@
 
 var queryresult;
+const connection = require('pg').Pool;
+const myconect = new connection({
+    user : 'zzdduyaaxgfqab',
+    host : 'ec2-174-129-225-160.compute-1.amazonaws.com',
+    database : 'dfd8gcsog7njl7',
+    password : '0493727bcbcc2f72994bbedb01f5bfe1360109bc5c66505f7c18dc47e2a1f151',
+    port : 5432,
+    ssl : on
+    });
 const connect =(query1) =>{
-    const connection = require('pg').Pool;
-    const myconect = new connection({
-        user : 'zzdduyaaxgfqab',
-        host : 'ec2-174-129-225-160.compute-1.amazonaws.com',
-        database : 'dfd8gcsog7njl7',
-        password : '0493727bcbcc2f72994bbedb01f5bfe1360109bc5c66505f7c18dc47e2a1f151',
-        port : 5432,
-        ssl : on
-        });
     myconect.query(query1,(err,result) =>{
     console.log(err,result)
     myconect.end()
