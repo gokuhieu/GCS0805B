29972ad8f9ff5ce26e5cbd452369f057cbb3ed13
gokuhieu <84957461+gokuhieu@users.noreply.github.com>
2021-06-28 12:57:22 +0700

    Update webcaculatorserver.js

--- webcaculatorserver.js
+++ webcaculatorserver.js
@@ -38,6 +38,9 @@ app.get('/addproduct/',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./home.html'))
 
 })
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./home.html'))
+})
 app.get('/home',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./home.html'))
 })
