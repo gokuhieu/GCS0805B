0963dc497923b9129d01e1b3350796fc338c8fb7
gokuhieu <84957461+gokuhieu@users.noreply.github.com>
2021-06-28 13:19:08 +0700

    Update webcaculatorserver.js

--- webcaculatorserver.js
+++ webcaculatorserver.js
@@ -53,9 +53,9 @@ app.get('/addcustomer',(req,res)=>{
 app.get('/addcategory',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./addcategory.html'))
 })
-var id
+
 app.get('/viewproduct',(req,res)=>{
-    res.render(path.join(__dirname,'./viewproduct.html'),{id = queryresult.id})
+    res.render(path.join(__dirname,'./viewproduct.html'),{id = queryresult})
 })
 
 app.listen(port, () => {
