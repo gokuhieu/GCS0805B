a151484bf701b14fc957133828bb6159b096d84a
gokuhieu <84957461+gokuhieu@users.noreply.github.com>
2021-07-02 12:24:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get('/addproduct',(req,res)=>{
                 return;
             }      
         })
-        res.render(path.resolve(__dirname,'./home.html'),{idp:1})
+        res.render(path.resolve(__dirname,'./home.html'),{idp:0})
         
     }
     else{
@@ -84,7 +84,7 @@ app.get('/addcategory',(req,res)=>{
                 return;
             }      
         })
-        res.render(path.resolve(__dirname,'./home.html',{idp:2}))
+        res.render(path.resolve(__dirname,'./home.html',{idp:0}))
         
     }
     else{
