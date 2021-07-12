128caa522582d5ea10bf78474954d77f8e2c331c
gokuhieu <84957461+gokuhieu@users.noreply.github.com>
2021-07-12 20:58:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -203,7 +203,7 @@ app.get('/checkout',(req,res)=>{
                         total= total+parseInt(result1.rows[i].price)*parseInt(result1.rows[i].quantity)
                         
                     }   
-                    query==`insert into public.invoice values('${data.invoiceid}','${data.invoicedate}','${total}')`;
+                    query=`insert into public.invoice values('${data.invoiceid}','${data.invoicedate}','${total}')`;
                     myconect.query(query,(err,result)=>{
                         if (err)
                         {
